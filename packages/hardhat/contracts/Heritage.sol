162a91b017df7b3b25757ba76c40e22846f5fe59
Yucel Olcay <yucel.olcay@cressoftconsultancy.com>
2023-11-30 16:09:59 +0700

    update heritage test

--- packages/hardhat/contracts/Heritage.sol
+++ packages/hardhat/contracts/Heritage.sol
@@ -21,52 +21,26 @@ contract Heritage is
 	address ethUsdPriceFeed;
 	uint public usdMinFeePerYer;
 	uint public feeThousandagePerYear;
-	address[] addressesToPay;
-	address[] subscribedAddresses;
-	struct Subscription {
-		address payingAddress;
-		uint startDate;
-		uint feePerYear;
-		uint feeThousandagePerYear;
-		uint payCount;
-		bool lastYearPaid;
-	}
-	mapping(address => Subscription) public addressSubscriptionMap;
 
 	// /// @custom:oz-upgrades-unsafe-allow constructor
 	// constructor() initializer {}
 
 	function initialize(
-		address owner,
 		address _heritageWalletAddr,
 		uint _usdMinFeePerYer,
 		uint _feeThousandagePerYear
 	) public initializer {
-		// __Ownable_init();
-		__Ownable_init(owner);
+		__Ownable_init(msg.sender);
+		heritageWalletAddr = _heritageWalletAddr;
 		usdMinFeePerYer = _usdMinFeePerYer;
 		feeThousandagePerYear = _feeThousandagePerYear;
-		heritageWalletAddr = _heritageWalletAddr;
 	}
 
 	function _authorizeUpgrade(
 		address newImplementation
-	) internal override onlyOwner {
+	) internal view override onlyOwner {
 		console.log("_authorizeUpgrade executed for admin:", _msgSender());
 		console.log("New implementation address:", newImplementation);
-	}
-
-	function subscribe() public {
-		console.log("Calculating the minimum fee for:", _msgSender());
-
-		Subscription storage newSubscription = addressSubscriptionMap[
-			_msgSender()
-		];
-
-		newSubscription.payingAddress = _msgSender();
-		newSubscription.startDate = block.timestamp;
-		newSubscription.feePerYear = usdMinFeePerYer;
-		newSubscription.feeThousandagePerYear = feeThousandagePerYear;
 	}
 
 	// temporary override
@@ -76,54 +50,42 @@ contract Heritage is
 	}
 
 	function deposit(address _addressToDeposit) external payable {
-		HeritageWallet wallet = _getHeritageWallet();
-
-		wallet.deposit(_addressToDeposit);
+		_getHeritageWallet().deposit(_addressToDeposit);
 	}
 
 	function sendFunds(uint amount, address payable receiver) external {
-		HeritageWallet wallet = _getHeritageWallet();
-
-		wallet.sendFunds(amount, receiver);
+		_getHeritageWallet().sendFunds(amount, receiver);
 	}
 
 	function payOutstandingFees(address _address) external returns (bool) {
-		HeritageWallet wallet = _getHeritageWallet();
-
-		return wallet.payOutstandingFees(_address);
+		return _getHeritageWallet().payOutstandingFees(_address);
 	}
 
 	function withdrawCollectedFees() external {
-		HeritageWallet wallet = _getHeritageWallet();
-
-		wallet.withdrawCollectedFees();
+		_getHeritageWallet().withdrawCollectedFees();
 	}
 
 	function distributeHeritage(address addr) external {
-		HeritageWallet wallet = _getHeritageWallet();
-
-		wallet.distributeHeritage(addr);
+		_getHeritageWallet().distributeHeritage(addr);
 	}
 
 	function addInheritant(address payable receiver, uint percentage) external {
-		HeritageWallet wallet = _getHeritageWallet();
-
-		wallet.addInheritant(receiver, percentage);
+		_getHeritageWallet().addInheritant(receiver, percentage);
 	}
 
 	function getRemainingInheritancePercentage(
 		address subscriber,
 		address receiver
 	) external view returns (uint, bool, uint) {
-		HeritageWallet wallet = _getHeritageWallet();
-
-		return wallet.getRemainingInheritancePercentage(subscriber, receiver);
+		return
+			_getHeritageWallet().getRemainingInheritancePercentage(
+				subscriber,
+				receiver
+			);
 	}
 
 	function calculateFeeToPay(address _address) external view returns (uint) {
-		HeritageWallet wallet = _getHeritageWallet();
-
-		return wallet.calculateFeeToPay(_address);
+		return _getHeritageWallet().calculateFeeToPay(_address);
 	}
 
 	function registerSubscriber(
@@ -131,9 +93,7 @@ contract Heritage is
 		uint _minFeePerYear,
 		uint _feeThousandagePerYear
 	) external {
-		HeritageWallet wallet = _getHeritageWallet();
-
-		wallet.registerSubscriber(
+		_getHeritageWallet().registerSubscriber(
 			_address,
 			_minFeePerYear,
 			_feeThousandagePerYear
@@ -141,9 +101,7 @@ contract Heritage is
 	}
 
 	function getEthPrice() external view returns (uint, uint) {
-		HeritageWallet wallet = _getHeritageWallet();
-
-		return wallet.getEthPrice();
+		return _getHeritageWallet().getEthPrice();
 	}
 
 	function _getHeritageWallet() internal view returns (HeritageWallet) {
--- packages/hardhat/test/Heritage.test.ts
+++ packages/hardhat/test/Heritage.test.ts
@@ -1,41 +1,84 @@
-// import { expect } from "chai";
+import { expect } from "chai";
 import { ethers, upgrades } from "hardhat";
-import { Heritage } from "../typechain-types";
+import { Heritage, HeritageWallet, Mock_AggregatorV3Interface } from "../typechain-types";
+import { Addressable } from "ethers";
+import { getImplementationAddress } from "@openzeppelin/upgrades-core";
 
-describe("Heritage", function () {
+describe("HeritageContract", function () {
   // We define a fixture to reuse the same setup in every test.
+  let heritageProxy: Heritage;
+  let ownerAddress: string | Addressable;
+  let heritageWalletAddr: string | Addressable;
+  let ethUsdFeedAddr: string | Addressable;
 
-  let heritageContract: Heritage;
-  let owner;
+  async function deployEthUsdFeedMock() {
+    console.info(`Deploying ETH-USD price feed mock.`);
+
+    const priceFeedMockFactory = await ethers.getContractFactory("Mock_AggregatorV3Interface");
+
+    const priceFeedMock = (await priceFeedMockFactory.deploy()) as unknown as Mock_AggregatorV3Interface;
+
+    await priceFeedMock.waitForDeployment();
+
+    console.info(`ETH-USD price feed mock deployed to ${priceFeedMock.target}.`);
+
+    ethUsdFeedAddr = priceFeedMock.target;
+  }
+
+  async function deployHeritageWallet() {
+    const heritageWalletFactory = await ethers.getContractFactory("HeritageWallet");
+
+    const heritageWallet = (await heritageWalletFactory.deploy(
+      ownerAddress,
+      ethUsdFeedAddr,
+    )) as unknown as HeritageWallet;
+
+    await heritageWallet.waitForDeployment();
+
+    console.info(`HeritageWallet is deployed at: ${heritageWallet.target}.`);
+
+    heritageWalletAddr = heritageWallet.target;
+  }
+
+  async function deployHeritage(usdMinFee = 5, feeThousandage = 1) {
+    const heritageFactory = await ethers.getContractFactory("Heritage");
+
+    const heritageContract = (await upgrades.deployProxy(
+      heritageFactory,
+      [heritageWalletAddr, usdMinFee, feeThousandage],
+      {
+        initializer: "initialize",
+        kind: "uups",
+      },
+    )) as unknown as Heritage;
+
+    console.log(`Heritage Proxy contract deployed to: ${heritageContract.target}.`);
+
+    const currentImplAddress = await getImplementationAddress(ethers.provider, heritageContract.target as string);
+
+    console.log(`Heritage Implementation contract deployed to: ${currentImplAddress}.`);
+
+    heritageProxy = heritageContract;
+  }
 
   before(async () => {
-    // const [owner] = await ethers.getSigners();
-    // const heritageFactory = await ethers.getContractFactory("Heritage");
-    // const proxy = await upgrades.deployProxy(heritageFactory, [owner.address], {
-    //   initializer: "initialize",
-    //   kind: "uups",
-    // });
-    // await proxy.waitForDeployment();
+    const [owner] = await ethers.getSigners();
+    ownerAddress = owner.address;
+
+    await deployEthUsdFeedMock();
+    await deployHeritageWallet();
   });
 
   describe("Deployment", function () {
     it("deploys", async () => {
-      const [from] = await ethers.getSigners();
-      owner = from;
-      const heritageFactory = await ethers.getContractFactory("Heritage");
+      const usdMinFee = 5;
+      const feeThousandage = 1;
 
-      heritageContract = (await upgrades.deployProxy(heritageFactory, [owner.address], {
-        initializer: "initialize",
-        kind: "uups",
-      })) as unknown as Heritage;
-
-      await heritageContract.waitForDeployment();
-
-      console.log(`Test contract deployed to: ${heritageContract.target} by: ${owner.address}`);
+      expect(await deployHeritage(usdMinFee, feeThousandage)).to.eql(undefined);
     });
 
-    it("Should have the right owner on deploy", async function () {
-      // expect(await heritageContract.owner()).to.equal(owner.address);
+    it("should have the correct owner", async function () {
+      expect(await heritageProxy.owner()).to.equal(ownerAddress);
     });
 
     // it("Should allow setting a new message", async function () {
